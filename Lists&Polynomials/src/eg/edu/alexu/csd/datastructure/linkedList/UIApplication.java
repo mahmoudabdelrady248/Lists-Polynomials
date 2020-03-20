6a06528be12376cb0cf21f97c3f98274a0558fd2
M.Abdelrady <M.Abdelrady@192.168.1.4>
2020-03-20 23:58:18 +0200

    Polynomials

--- Lists&Polynomials/src/eg/edu/alexu/csd/datastructure/linkedList/UIApplication.java
+++ Lists&Polynomials/src/eg/edu/alexu/csd/datastructure/linkedList/UIApplication.java
@@ -13,7 +13,7 @@ public class UIApplication {
 	    	System.out.println("5_ Multiply two polynomials");
 	    	System.out.println("6_ Evaluate a polynomial variable");
 	    	System.out.println("7_ Clear a polynomial variable");
-	    	Scanner input=new Scanner(System.in);
+	     	Scanner input=new Scanner(System.in);
 	    	int num = 0;
 	    	try {
 	    	 num=input.nextInt();
@@ -25,121 +25,37 @@ public class UIApplication {
 	    		System.out.println("invalid input");
 	    		input.next();
 	    	}
+	    	
+	    	
 	    	switch (num) {
+	    	
 	    	case 1:
 	    		System.out.println("Insert the variable name: A, B or C");
-	    		char poly1;
-	    		try {
-	    		poly1=input.next().charAt(0);	    	
-	    		System.out.printf("Insert the polynomial terms in the form:\n(coeff1,exponent1),(coeff2,exponent2),...\n");
-	    		Scanner o=new Scanner(System.in);
 	    		
-	    	    String a;
+char poly1;
+	    		
+	    		try {
+	    			
+	    		poly1=input.next().charAt(0);
+	    		if (poly1!='A'&& poly1!='B'&&poly1!='C'&&poly1!='a'&& poly1!='b'&&poly1!='c')  {
+	    			System.out.println("invalid input");
+	    			break;
+	    		}
+	    		System.out.printf("Insert the polynomial terms in the form:\n(coeff1,exponent1),(coeff2,exponent2),...\n");
+	    		Scanner ss=new Scanner(System.in);
+	    	    String b=null;
 	    	    try {
-	    	    a=o.nextLine();String []x=a.trim().split("\\s+");
+	    	    b=ss.nextLine();String []x=b.trim().split("\\s+");
 	    	    String c="";for(int i=0;i<x.length;i++) {c+=x[i];}
-	    	    String []v=c.split(",");int[][] u=new int[v.length/2][2];int r=0;
-	    	    for(int i=0;i<v.length;i++) {v[i]=v[i].replaceAll("[()]", "");
-	    	    v[i]=v[i].replaceAll("\\s+", "");}
-	    	 for(int i=0;i<v.length/2;i++) {
-	    	 	for(int j=0;j<2;j++) {u[i][j]=Integer.parseInt(v[r++]);	
+	    	    String []v1=c.split(",");int[][] u=new int[v1.length/2][2];int r=0;
+	    	    for(int i=0;i<v1.length;i++) {v1[i]=v1[i].replaceAll("[()]", "");
+	    	    v1[i]=v1[i].replaceAll("\\s+", "");}
+	    	 for(int i=0;i<v1.length/2;i++) {
+	    	 	for(int j=0;j<2;j++) {u[i][j]=Integer.parseInt(v1[r++]);	
 	    	 		}}
                s.setPolynomial(poly1,u);break;
-	    	    }catch(Exception e) {
-	    			System.out.println("invalid input");
-	    			break;
-	    			}
-	    		}
-	    		catch(Exception e) {
-	    			System.out.println("invalid input");
-	    			break;
-	    		}
-	    		case 2:
-	    		System.out.println("Insert the variable name: A, B, C OR R");
-	    		char poly2;
-	    		try {
-	    		poly2=input.next().charAt(0);
-                if(poly2=='A') {
-	    		System.out.println(s.print(poly2));System.out.println("polynomial A is set");}
-                if(poly2=='B') {
-    	    		System.out.println(s.print(poly2));System.out.println("polynomial B is set");}
-                if(poly2=='C') {
-    	    		System.out.println(s.print(poly2));System.out.println("polynomial C is set");}
-                if(poly2=='R') {
-    	    		System.out.println(s.print(poly2));System.out.println("polynomial R is set");}break;
-	    		}catch(Exception e) {
-	    			System.out.println("invalid input");
-	    			break;
-	    		}
-	    		case 3:
-	    		System.out.println("Insert first operand variable name:A ,B or c");
-	    		char poly3;
-	    		try {
-	    		poly3=input.next().charAt(0); 	    
-	    		System.out.println("Insert second operand variable name:A ,B or c");
-	    		try {
-		    		char poly4=input.next().charAt(0);
-	    		int [][]a1=s.add(poly3, poly4);String str1=new String();
-	    		s.setPolynomial('R', a1);
-	    		for(int i=0;i<a1.length;i++) {str1+="(";str1+=a1[i][0];str1+=",";str1+=a1[i][1];str1+=")";
-	    		if(i==a1.length-1) {}
-	    		else {str1+=",";}
-	    		}
-	    		System.out.println("Result set in R is : "+ str1);
-	    		break;}
-	    		catch(Exception e) {
-	    			System.out.println("invalid input");
-	    			break;
-	    		}
-	    		}
-	    		catch(Exception e) {
-	    			System.out.println("invalid input");
-	    			break;
-	    		}
-	    	case 4:
-	    		System.out.println("Insert first operand variable name:A ,B or c");
-	    		char poly5;
-	    		try {
-	    		poly5=input.next().charAt(0); 
-	    		System.out.println("Insert second operand variable name:A ,B or c");
-	    		char poly6;
-	    		try {
-	    		poly6=input.next().charAt(0); 
-	    		int [][]a2=s.subtract(poly5, poly6);String str2=new String();
-	    		s.setPolynomial('R', a2);
-	    		for(int i=0;i<a2.length;i++) {str2+="(";str2+=a2[i][0];str2+=",";str2+=a2[i][1];str2+=")";
-	    		if(i==a2.length-1) {}
-	    		else {str2+=",";}
-	    		}
-	    		System.out.println("Result set in R is : "+ str2);break;
-	    		}
-	    		catch(Exception e) {
-	    			System.out.println("invalid input");
-	    			break;
-	    		}
-	    		}
-	    		catch(Exception e) {
-	    			System.out.println("invalid input");
-	    			break;
-	    		}
-	    	case 5:
-	    		System.out.println("Insert first operand variable name:A ,B or c");
-	    		char poly7;
-	    		try {
-	    		poly7=input.next().charAt(0); 
-	    		System.out.println("Insert second operand variable name:A ,B or c");
-	    		char poly8;
-	    		try {
-	    		poly8=input.next().charAt(0); 
-	    		int [][]a3=s.multiply(poly7, poly8);String str3=new String();
-	    		s.setPolynomial('R', a3);
-	    		for(int i=0;i<a3.length;i++) {str3+="(";str3+=a3[i][0];str3+=",";str3+=a3[i][1];str3+=")";
-	    		if(i==a3.length-1) {}
-	    		else {str3+=",";}
-	    		}
-	    		System.out.println("Result set in R is : "+ str3);break;
-	    		}
-	    		catch(Exception e) {
+	    	    }
+	    	    catch(Exception e) {
 	    			System.out.println("invalid input");
 	    			break;
 	    			
@@ -149,30 +65,155 @@ public class UIApplication {
 	    			System.out.println("invalid input");
 	    			break;
 	    		}
+	    		case 2:
+	    		System.out.println("Insert the variable name: A, B, C OR R");
+	    		
+        char poly2 = 0;
+	    		
+	    		try {
+	    			
+	    		poly1=input.next().charAt(0);
+	    		if (poly2!='A'&& poly2!='B'&&poly2!='C'&& poly2!='R'&&poly2!='a'&& poly2!='b'&&poly2!='c')  {
+	    			System.out.println("invalid input");
+	    			break;
+	    		}
+	    		s.print(poly2);break;
+	    		}
+	    		catch(Exception e) {
+	    			System.out.println("invalid input");
+	    		}
+	    		case 3:
+	    		System.out.println("Insert first operand variable name:A ,B or c");
+char poly3 = 0;
+	    		
+	    		try {
+	    			
+	    		poly1=input.next().charAt(0);
+	    		if (poly3!='A'&& poly3!='B'&&poly3!='C'&&poly3!='a'&& poly3!='b'&&poly3!='c')  {
+	    			System.out.println("invalid input");
+	    			break;
+	    		}
+	    		System.out.println("Insert second operand variable name:A ,B or c");
+char poly4 = 0;
+	    		
+	    		try {
+	    			
+	    		poly1=input.next().charAt(0);
+	    		if (poly4!='A'&& poly4!='B'&&poly4!='C'&&poly4!='a'&& poly4!='b'&&poly4!='c')  {
+	    			System.out.println("invalid input");
+	    			break;
+	    		} 
+	    		s.add(poly3, poly4);break;
+	    		}
+	    		catch(Exception e) {
+	    			System.out.println("invalid input");
+	    		}
+	    		}
+	    		catch(Exception e) {
+	    			System.out.println("invalid input");
+	    		}
+	    	case 4:
+	    		System.out.println("Insert first operand variable name:A ,B or c");
+char poly5 = 0;
+	    		
+	    		try {
+	    			
+	    		poly1=input.next().charAt(0);
+	    		if (poly5!='A'&& poly5!='B'&&poly5!='C'&&poly5!='a'&& poly5!='b'&&poly5!='c')  {
+	    			System.out.println("invalid input");
+	    			break;
+	    		} 
+	    		System.out.println("Insert second operand variable name:A ,B or c");
+char poly6 = 0;
+	    		
+	    		try {
+	    			
+	    		poly1=input.next().charAt(0);
+	    		if (poly6!='A'&& poly6!='B'&&poly6!='C'&&poly6!='a'&& poly6!='b'&&poly6!='c')  {
+	    			System.out.println("invalid input");
+	    			break;
+	    		} 
+	    		s.subtract(poly5, poly6);break;
+	    		}
+	    		catch(Exception e) {
+	    			System.out.println("invalid input");
+	    		}
+	    		}
+	    		catch(Exception e) {
+	    			System.out.println("invalid input");
+	    		}
+	    	case 5:
+	    		System.out.println("Insert first operand variable name:A ,B or c");
+char poly7 = 0;
+	    		
+	    		try {
+	    			
+	    		poly1=input.next().charAt(0);
+	    		if (poly7!='A'&& poly7!='B'&&poly7!='C'&&poly7!='a'&& poly7!='b'&&poly7!='c')  {
+	    			System.out.println("invalid input");
+	    			break;
+	    		}
+	    		System.out.println("Insert second operand variable name:A ,B or c");
+char poly8 = 0;
+	    		
+	    		try {
+	    			
+	    		poly1=input.next().charAt(0);
+	    		if (poly8!='A'&& poly8!='B'&&poly8!='C'&&poly8!='a'&& poly8!='b'&&poly8!='c')  {
+	    			System.out.println("invalid input");
+	    			break;
+	    		}
+	    		s.multiply(poly7, poly8);break;
+	    		}
+	    		catch(Exception e) {
+	    			System.out.println("invalid input");
+	    		}
+	    		}
+	    		catch(Exception e) {
+	    			System.out.println("invalid input");
+	    		}
 	    	case 6:	
 	    		System.out.println("Insert the variable name: A, B, C OR R");
-	    		char poly9;
+	    		
+char poly9 = 0;
+	    		
 	    		try {
-	    		poly9=input.next().charAt(0);
+	    			
+	    		poly1=input.next().charAt(0);
+	    		if (poly9!='A'&& poly9!='B'&&poly9!='C'&&poly9!='a'&& poly9!='b'&&poly9!='c')  {
+	    			System.out.println("invalid input");
+	    			break;
+	    		}
 	    		System.out.println("Enter value you want");
 	    		float value=input.nextFloat();
 	    	    System.out.println(s.evaluatePolynomial(poly9, value));break;
 	    		}
 	    		catch(Exception e) {
 	    			System.out.println("invalid input");
-	    		
-	    			break;}
+	    		}
 	    		case 7:
 	    		System.out.println("Insert the variable name: A, B, C OR R");
-	    		char poly10;
+	    		
+char poly10 = 0;
+	    		
 	    		try {
-		    		poly10=input.next().charAt(0);
+	    			
+	    		poly1=input.next().charAt(0);
+	    		if (poly10!='A'&& poly10!='B'&&poly10!='C'&&poly10!='a'&& poly10!='b'&&poly10!='c')  {
+	    			System.out.println("invalid input");
+	    			break;
+	    		}
 	    		s.clearPolynomial(poly10);break;
 	    		}
 	    		catch(Exception e) {
 	    			System.out.println("invalid input");
-	    		break;
 	    		}
+	    		//default:
+	    			
+	    			
+	        		//	System.out.println("invalid input");
+	        			
+	        	
 	        }
 	    	
 	    	}
